49b1be60839d6197d5ab621280429df8572565ba
RavenKZP <mkruk96@gmail.com>
2026-07-23 22:24:53 +0200

    Detect water height and lava in Hooks
    Introduce ToLower and get_water_height (based on Splashes-of-Skyrim) which returns a pair<float,bool> for water height and isLava (determined via editorID). Update ProcessInWaterHook to call get_water_height and avoid killing actors when the water is lava.

--- include/Hooks.h
+++ include/Hooks.h
@@ -1,6 +1,58 @@
 #pragma once
 
 #include "Settings.h"
+#include "ClibUtil/editorID.hpp"
+
+std::string ToLower(std::string s) {
+    std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c) { return static_cast<char>(std::tolower(c)); });
+    return s;
+}
+
+// Based on po3's Splashes-of-Skyrim
+// https://github.com/powerof3/Splashes-of-Skyrim
+// water height, isLava
+std::pair<float, bool> get_water_height(const RE::NiPoint3& a_pos) {
+    float waterHeight = -RE::NI_INFINITY;
+    bool isLava = false;
+
+    if (const auto waterManager = RE::TESWaterSystem::GetSingleton()) {
+        const RE::BSSpinLockGuard locker(waterManager->lock);
+
+        for (const auto& waterObjectPtr : waterManager->waterObjects) {
+            const auto waterObject = waterObjectPtr.get();
+            if (!waterObject) {
+                continue;
+            }
+            for (const auto& boundPtr : waterObject->multiBounds) {
+                const auto bound = boundPtr.get();
+                if (!bound) {
+                    continue;
+                }
+                if (auto size{bound->size}; size.z <= 10.0f) {  // avoid sloped water
+                    auto center{bound->center};
+                    const auto boundMin = center - size;
+                    const auto boundMax = center + size;
+                    if (!(a_pos.x < boundMin.x || a_pos.x > boundMax.x || a_pos.y < boundMin.y ||
+                          a_pos.y > boundMax.y)) {
+                        waterHeight = center.z;
+
+                        bool lava = false;
+                        auto editorID = clib_util::editorID::get_editorID(waterObject->waterType);
+                        if (!editorID.empty()) {
+                            std::string lowerStr = ToLower(editorID);
+                            if (lowerStr.find("lava") != std::string::npos) {
+                                lava = true;
+                            }
+                        }
+                        return std::make_pair(waterHeight, lava);
+                    }
+                }
+            }
+        }
+    }
+
+    return std::make_pair(-RE::NI_INFINITY, false);
+}
 
 namespace Hooks {
     struct ProcessInWaterHook {
@@ -14,8 +66,8 @@ namespace Hooks {
             if (a_this->HasKeywordString("Douse_KillByWater")) {
                 float actorHeight = a_this->GetHeight();
                 auto actorPos = a_this->GetPosition();
-                auto waterHeight = a_this->GetWaterHeight();
-                if (actorPos.z + (actorHeight * Settings::GetSingleton()->Submerged) < waterHeight) {
+                auto [waterHeight, isLava] = get_water_height(actorPos);
+                if (!isLava && actorPos.z + (actorHeight * Settings::GetSingleton()->Submerged) < waterHeight) {
                     a_this->DoDamage(99999, nullptr, true);
                 }
             }
